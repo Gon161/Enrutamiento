39dd177bf3101b03391c3b98ada3bed5243e8add
GON161 <82066172+Gon161@users.noreply.github.com>
2024-10-25 00:37:38 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        public class Cargador { 
+        public Dictionary<int, string> Diccionario = new Dictionary<int, string>();
+            public Cargador()
+            {
+                
+            }
+            public void RellenarDiccionario(int Id,string valor)
+            {
+                Diccionario[Id] = valor;
+                Console.WriteLine("Dato guardado");
+            }
+        }
+
+        public  void AlumnM()
+        {
+
+        }
+
+        static void Main(string[] args)
+        {
+            Cargador cargador = new Cargador();
+            Console.WriteLine("Ingresa una url");
+            Console.ReadLine();
+            string url = Console.ReadLine();
+            cargador.RellenarDiccionario(0,url);
+
+
+            Console.ReadKey();
+        }
+    }
+}
